88675f9510879b866014aea926b6ebd9d248d8c1
Divaweb <lanittad@hotmail.com>
2018-10-13 20:19:11 -0700

    corrections

--- lib/kele.rb
+++ lib/kele.rb
@@ -9,16 +9,20 @@ class Kele
   base_uri 'https://www.bloc.io/api/v1'
 
   def initialize(email, password)
-    post_response = self.class.post('/sessions', body: {
-      email: email,
-      password: password
-      })
-      @user_auth_token = post_response['auth_token']
-      raise "Invalid Email or Password. Try again." if @user_auth_token.nil?
+
+    @email = email
+    @password = password
+
+    response = self.class.post('https://www.bloc.io/api/v1/sessions', body: { "email": @email, "password": @password})
+    @auth_token = response[ "auth_token" ]
+
+    if @auth_token.nil? || response.nil?
+      raise Error, "Unable to access user. Please try again with valid user information."
+    end
   end
 
   def get_me
-    response = self.class.get('/user/me', headers: { "authorizatio" => @user_auth_token })
+    response = self.class.get('https://www.bloc.io/api/v1/users/me', headers: { "authorizatio" => @user_auth_token })
     JSON.parse(response.body)
   end
 
@@ -28,21 +32,38 @@ class Kele
     JSON.parse(response.body)
   end
 
-  def get_messages(page = nil) # optional method parameter
-    if page != nil # return specified page
-      response = self.class.get("https://www.bloc.io/api/v1/message_threads", headers: { "authorization": @auth_token }, body: { "page": page })
-    else # return first page
-      response = self.class.get("https://www.bloc.io/api/v1/message_threads", headers: { "authorization": @auth_token })
+  def get_messages(page_number=0)
+    if page_number == 0
+      url = "/message_threads"
+      response = self.class.get(url, headers: { "authorization" => @auth_token })
+    else
+      url = "/message_threads?page=#{page_number}"
+      response = self.class.get(url, headers: { "authorization" => @auth_token }, body: { page: page_number })
     end
-    JSON.parse(response.body)
+
+    if response.success?
+      JSON.parse(response.body)
+    else
+      raise response.response
+    end
   end
-   def create_message(sender, recipient_id, subject, stripped_text)
-    response = self.class.post("https://www.bloc.io/api/v1/messages", headers: { "authorization": @auth_token },
+
+  def create_message(subject, text, recipient=@mentor_id)
+    response = self.class.post("/messages", headers: { "authorization" => @auth_token }, body: {
+      "sender" => @email,
+      "recipient_id" => recipient,
+      "subject" => subject,
+      "stripped-text" => text
+    }) 
+
+  def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment, enrollment_id)
+    response = self.class.post("https://www.bloc.io/api/v1/checkpoint_submissions", headers: { "authorization": @auth_token },
       body: {
-        "sender": sender,
-        "recipient_id": recipient_id,
-        "subject": subject,
-        "stripped-text": stripped_text
-      })
+        "checkpoint_id": checkpoint_id,
+        "assignment_branch": assignment_branch,
+        "assignment_commit_link": assignment_commit_link,
+        "comment": comment,
+        "enrollment_id": enrollment_id
+        })
   end
 end
